624b6c01149325b056cf16475aa5001d29803b07
Takashi Nakagawa <hawk4.n@gmail.com>
2017-10-30 09:49:02 +0900

    try

--- src/tools.cpp
+++ src/tools.cpp
@@ -3,7 +3,6 @@
 
 using Eigen::VectorXd;
 using Eigen::MatrixXd;
-using std::vector;
 
 Tools::Tools() {}
 
@@ -11,8 +10,21 @@ Tools::~Tools() {}
 
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
-  /**
-  TODO:
-    * Calculate the RMSE here.
-  */
+  VectorXd rmse(4);
+  rmse << 0,0,0,0;
+
+  if(estimations.size() != ground_truth.size() || estimations.size() == 0) {
+    std::cout << "Invalid estimiation or ground truth data." << std::endl;
+    return rmse;
+  }
+
+  for (auto i = 0; i < estimations.size(); ++i) {
+    VectorXd residual = estimations[i] - ground_truth[i];
+    residual = residual.array() * residual.array();
+    rmse += residual;
+  }
+
+  rmse /= estimations.size();
+  rmse = rmse.array().sqrt();
+  return rmse;
 }
